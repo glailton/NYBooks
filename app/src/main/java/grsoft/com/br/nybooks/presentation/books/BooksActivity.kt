27d48c89112cf5858639e5f67a95a71b314445d3
Glailton Costa <glailton.rc@gmail.com>
2020-12-18 00:13:39 -0300

    Refactoring initial project

--- app/src/main/java/grsoft/com/br/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/grsoft/com/br/nybooks/presentation/books/BooksActivity.kt
@@ -1,11 +1,12 @@
-package grsoft.com.br.nybooks
+package grsoft.com.br.nybooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import grsoft.com.br.nybooks.R
 
-class MainActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_books)
     }
 }
